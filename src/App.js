754d1ca1c528f3c0c606c8ad9ab183e9f2b0c225
Mark Sirany <marksirany@me.com>
2020-01-22 14:26:52 -0600

    made paddle 2 different color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./styles.css";
+
 import Paddle from "./components/Paddle";
+import Paddle2 from "./components/Paddle2";
 
 export default function App() {
   return (
     <div className="App">
       <Paddle />
-      <Paddle />
+      <Paddle2 />
     </div>
   );
 }
---
+++ src/components/Paddle2.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Paddle2.css";
+
+export default function paddle() {
+  return <div className="paddle2" />;
+}
